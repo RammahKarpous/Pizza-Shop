12ac600a434b0b417265884ca24686565af4f053
rammah karpous <rammah@iweb.co.uk>
2020-02-05 08:31:03 +0000

    Added the banner content

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
-@section('title', 'Home page')
+@section('title', 'PizzaPalace')
 
 @section('content')
-    <h1>Home page</h1>
+
+    <div class="banner">
+        <img src="" alt="PizzaPalace banner image" class="banner__image">
+        <div class="banner__cta">
+            <h1 class="banner__heading heading-1">Create your custom pizza</h1>
+            <p class="banner__text small-text">Let your creativity unleash itself and create a bad boy pizza!</p>
+            <a href="" class="banner__button button">Get started</a>
+        </div>
+    </div>
 @endsection
