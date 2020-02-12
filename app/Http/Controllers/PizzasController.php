32981cdfaa140191ec7971df91eeb10fe1832f78
rammah karpous <rammah@iweb.co.uk>
2020-02-12 21:07:19 +0000

    ran cmd+alt + L

--- app/Http/Controllers/PizzasController.php
+++ app/Http/Controllers/PizzasController.php
@@ -5,13 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Pizza;
 
-class PizzasController extends Controller
-{
+class PizzasController extends Controller {
     public function pizza( $slug ) {
-        $pizza = Pizza::where('slug', $slug)->first();
+        $pizza = Pizza::where( 'slug', $slug )->first();
 
-        return view('pizzas.pizza', [
+        return view( 'pizzas.pizza', [
             'pizza' => $pizza
-        ]);
+        ] );
     }
 }
