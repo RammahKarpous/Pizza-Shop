c8e1242321e92c952e65a7e65b292ee27dcc8638
rammah karpous <rammah@iweb.co.uk>
2020-02-14 19:43:26 +0000

    Added the checkbox-checked and checkbox-unchecked images, and customised the styling

--- app/Http/Controllers/CustomPizzasController.php
+++ app/Http/Controllers/CustomPizzasController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\CustomPizza;
+use App\Topping;
+use App\Size;
 
 class CustomPizzasController extends Controller
 {
     public function customPizza() {
-        return view('pizzas.create');
+        $toppings = Topping::all();
+        $sizes = Size::all();
+
+        return view('pizzas.create', [
+            'toppings' => $toppings,
+            'sizes' => $sizes
+        ]);
     }
 
     public function add( Request $request ) {
-        
+
     }
 }
--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -5,7 +5,7 @@
         <h1 class="heading-1 black-color">Create your custom pizza</h1>
         <div class="grid grid-2 custom-pizza">
             <div class="custom-pizza__left-column">
-                <h2 class="heading-2 black-color custom-pizza__name placeholder-style">Your custom pizza name</h2>
+                <h2 class="black-color custom-pizza__name placeholder-style">Your custom pizza name</h2>
 
                 <div class="custom-pizza__topping-images">
                     <div class="pizza-toppings"></div>
@@ -15,25 +15,46 @@
 
             <div class="custom-pizza__right-column">
                 <h2 class="heading-3 black-color">Pizza details</h2>
-                {!! Form::open(['method' => 'POST', 'action' => 'CustomPizzasController@add', 'class' => 'form form__custom-pizza']) !!}
+                {!! Form::open(['method' => 'POST', 'action' => 'CustomPizzasController@add', 'class' => 'form form__custom-pizza top-m']) !!}
                     <div class="form__form-group">
                         {{ Form::label('pizza_name', 'Pizza Name', ['class' => 'small-text']) }}
                         {{ Form::text('pizza_name', '', ['class' => 'form__input custom-pizza__input-field form__input--input-field gray-border top-m', 'maxlength' => 20]) }}
                     </div>
                 {!! Form::close() !!}
 
-                <div class="section m-top flex flex-row topping-tags">
+                <span class="label top-m block">Sizes</span>
+                <div class="section top-m flex flex-row size-tags">
+                    @if(count($sizes) > 0)
+                        @foreach($sizes as $size)
+                            <div class="form-field tag-wrapper">
+                                <input type="radio" name="sizes" id="{{ $size->size  }}">
+                                <label class="tag-label" for="{{ $size->size  }}">{{ $size->size }}</label>
+                            </div>
+                        @endforeach
+                        @else
+                        <p>There are no sizes available</p>
+                    @endif
+                </div>
+
+                <div class="section top-m flex flex-row topping-tags">
                     <span class="label">Toppings</span>
 
-                    <div class="form-field topping-tag-wrapper">
-                        <input type="checkbox" name="toppings" id="tomato-sauce">
-                        <label class="topping-checkbox topping-checkbox--disabled" for="tomato-sauce">Tomato Sauce</label>
+                    <div class="form-field tag-wrapper">
+                        <input type="checkbox" name="toppings" id="tomato-sauce" disabled checked>
+                        <label class="tag-label tag-label--disabled" for="tomato-sauce">Tomato Sauce</label>
                     </div>
 
-                    <div class="form-field topping-tag-wrapper">
+                    <div class="form-field tag-wrapper">
                         <input type="checkbox" name="toppings" id="cheese" disabled checked>
-                        <label class="topping-checkbox-label topping-checkbox--disabled" for="cheese">Cheese</label>
+                        <label class="tag-label tag-label--disabled" for="cheese">Cheese</label>
                     </div>
+
+                    @foreach($toppings as $topping)
+                        <div class="form-field tag-wrapper">
+                            <input type="checkbox" name="toppings" id="{{ $topping->topping  }}">
+                            <label class="tag-label" for="{{ $topping->topping  }}">{{ $topping->name }}</label>
+                        </div>
+                    @endforeach
                 </div>
             </div>
 
